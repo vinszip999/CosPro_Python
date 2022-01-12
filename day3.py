bf249562614eb7e4cc014917ea92f5119ae98f32
vinszip999 <w2011@e-mirim.hs.kr>
2022-01-12 10:20:26 +0900

    [CosPro-Python 3일차 (문제19)]

--- day3.py
+++ day3.py
@@ -50,16 +50,37 @@
 # 문제 18
 # 이름에 특정 문자가 포함된 사람의 수를 구하는 함수 수정하기
 
-def solution(name_list):
+# def solution(name_list):
+#     answer = 0
+#     for name in name_list:
+#         for n in name:
+#             if n == 'j' or n == 'k':
+#                 answer += 1
+#                 break
+#     return answer
+#
+# names = ['james', 'kim john', 'jokin']
+# ret = solution(names)
+#
+# print("solution 함수의 반환 값은 ", ret, "입니다.")
+
+
+# 문제 19
+# 거스름돈을 계산하는 함수 작성하기
+
+def solution(price, money):
     answer = 0
-    for name in name_list:
-        for n in name:
-            if n == 'j' or n == 'k':
-                answer += 1
-                break
+    total = 0
+    for i in price:
+        total += i
+    answer = money-total
+    if answer < 0:
+        answer = -1
     return answer
 
-names = ['james', 'kim john', 'jokin']
-ret = solution(names)
+price = [1000, 5000, 500, 2000]
+money = 10000
+ret = solution(price, money)
 
 print("solution 함수의 반환 값은 ", ret, "입니다.")
+
