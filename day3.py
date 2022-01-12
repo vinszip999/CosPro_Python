ff576c5c5fe9eedaed67aa308de61a26a6cc62e3
vinszip999 <w2011@e-mirim.hs.kr>
2022-01-12 09:43:39 +0900

    [CosPro-Python 3일차 (문제17)]

--- day3.py
+++ day3.py
@@ -26,3 +26,22 @@ ret = solution(height, k)
 
 print("solution 함수의 반환 값은 ", ret, "입니다.")
 
+
+# 문제 17
+# 문자열 내 특정 문자를 바꾸는 함수 수정하기
+
+def solution(s):
+    s_list = list(s)
+    n = len(s)
+    for i in range(n):
+        if s_list[i] == 'a':
+            s_list[i] = 'z'
+        elif s_list[i] == 'z':
+            s_list[i] = 'a'
+
+    return "".join(s_list)
+
+name = 'james'
+ret = solution(name)
+
+print("solution 함수의 반환 값은 ", ret, "입니다.")
