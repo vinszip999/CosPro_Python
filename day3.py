d4d7f953f7304602ab48ff08c66542518ed6fc7e
vinszip999 <w2011@e-mirim.hs.kr>
2022-01-12 09:52:10 +0900

    [CosPro-Python 3일차 (문제18)]

--- day3.py
+++ day3.py
@@ -1,47 +1,65 @@
 # 문제 16
 # 키가 큰 사람의 수를 구하는 함수 수정하기
 
-def solution(height, k):
-    answer = 0
-    n = len(height)
-
-    # 첫 번째 방법
-    for h in height:
-        if h > k:
-            answer += 1
-    return answer
-
-    # 두 번째 방법
-    # index = 0
-    # while index < n:
-    #     if height[index] > k:
-    #         answer += 1
-    #     index += 1
-    # return answer
-
-
-height = [165, 170, 175, 180, 184]
-k = 175
-ret = solution(height, k)
-
-print("solution 함수의 반환 값은 ", ret, "입니다.")
+# def solution(height, k):
+#     answer = 0
+#     n = len(height)
+#
+#     # 첫 번째 방법
+#     for h in height:
+#         if h > k:
+#             answer += 1
+#     return answer
+#
+#     # 두 번째 방법
+#     # index = 0
+#     # while index < n:
+#     #     if height[index] > k:
+#     #         answer += 1
+#     #     index += 1
+#     # return answer
+#
+#
+# height = [165, 170, 175, 180, 184]
+# k = 175
+# ret = solution(height, k)
+#
+# print("solution 함수의 반환 값은 ", ret, "입니다.")
 
 
 # 문제 17
 # 문자열 내 특정 문자를 바꾸는 함수 수정하기
 
-def solution(s):
-    s_list = list(s)
-    n = len(s)
-    for i in range(n):
-        if s_list[i] == 'a':
-            s_list[i] = 'z'
-        elif s_list[i] == 'z':
-            s_list[i] = 'a'
+# def solution(s):
+#     s_list = list(s)
+#     n = len(s)
+#     for i in range(n):
+#         if s_list[i] == 'a':
+#             s_list[i] = 'z'
+#         elif s_list[i] == 'z':
+#             s_list[i] = 'a'
+#
+#     return "".join(s_list)
+#
+# name = 'james'
+# ret = solution(name)
+#
+# print("solution 함수의 반환 값은 ", ret, "입니다.")
 
-    return "".join(s_list)
 
-name = 'james'
-ret = solution(name)
+# 문제 18
+# 이름에 특정 문자가 포함된 사람의 수를 구하는 함수 수정하기
+
+def solution(name_list):
+    answer = 0
+    for name in name_list:
+        for n in name:
+            if n == 'j' or n == 'k':
+                answer += 1
+                break
+    return answer
+
+names = ['james', 'kim john', 'jokin']
+ret = solution(names)
 
 print("solution 함수의 반환 값은 ", ret, "입니다.")
