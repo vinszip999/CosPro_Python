5ef4fec0900dc53d7b2ad4061e644a6be11356ad
vinszip999 <w2011@e-mirim.hs.kr>
2022-01-13 14:46:21 +0900

    [모의고사 4회(1~6)]

--- codingTest_4.py
+++ codingTest_4.py
@@ -11,12 +11,29 @@
 
 
 # 03 통학버스 왕복 횟수 구하는 함수 작성하기
-def solution(student, apts):
-    result = 0
-    total = 0
-    for i in student:
-        total += i
-    result = total // 12
-    if total % 12 != 0:
-        result += 1
-    return result
+# def solution(student, apts):
+#     result = 0
+#     total = 0
+#     for i in student:
+#         total += i
+#     result = total // 12
+#     if total % 12 != 0:
+#         result += 1
+#     return result
+
+
+# 04 열린 문의 개수를 구하는 함수 빈칸 채우기
+# 1. range(m + 1)
+# 2. 1 - room[k]  # 1-> 0, 0 -> 1
+# 3. OPEN
+
+
+# 05 대괄호 쌍의 개수를 세는 함수 빈칸 채우기
+# 1. False
+# 2. True
+
+
+# 06 수업별 신청자 인원수 구하는 함수 수정하기
+# answer[i] += arr[i+k]
+# -->
+# answer[i] += arr[i*4+k]  # 과목별로 4개씩
